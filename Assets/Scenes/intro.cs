079a2316ce0d68b2158113ad5993a274bfacde20
AntFranco <103537099+AntFranco@users.noreply.github.com>
2023-08-20 12:36:02 -0700

    Initial Menu

--- Assets/Scenes/intro.cs
+++ Assets/Scenes/intro.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class intro : MonoBehaviour
 {
+    public GameObject settingsMenu; 
+        
+    public void EnableSettings()
+    {
+        settingsMenu.SetActive(true);     
+    }
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
