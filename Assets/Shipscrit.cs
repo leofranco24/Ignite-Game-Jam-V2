42b9984e2c286482b85805a6569fb22703e414f5
AntFranco <103537099+AntFranco@users.noreply.github.com>
2023-08-19 20:43:35 -0700

    main menu

---
+++ Assets/Shipscrit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shipscrit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        t
+        //gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
